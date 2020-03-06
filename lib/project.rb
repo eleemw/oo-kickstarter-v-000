3595286ff32ef3fe706004bcaa045584b7ba7807
Erica Murdock-Waters <ericahpa@gmail.com>
2020-03-06 18:32:49 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -9,7 +9,7 @@ class Project
   end
 
   def add_backer(backer)  #accepts Backer argument and stores it in backers array
-    @backers << backer
+    self.backers << backer
     backer.back_project(self) #adds the project to the backer's backed_projects array
   end
 end
